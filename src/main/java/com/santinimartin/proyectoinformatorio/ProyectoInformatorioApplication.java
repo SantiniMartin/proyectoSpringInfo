15504c67eb927cb1bd658cec1858ed41fc5f2d89
Martin Santini <Martin_Santini219@outlook.es>
2024-09-26 03:52:43 -0300

    Inicio del proyecto

---
+++ src/main/java/com/santinimartin/proyectoinformatorio/ProyectoInformatorioApplication.java
@@ -0,0 +1,13 @@
+package com.santinimartin.proyectoinformatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInformatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInformatorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santinimartin/proyectoinformatorio/ProyectoInformatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santinimartin.proyectoinformatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInformatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
